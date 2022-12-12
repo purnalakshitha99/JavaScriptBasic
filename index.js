11b672374b6f1992df27827b8d739b4d20ed75f2
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2022-12-13 04:32:46 +0530

    equlation

--- index.js
+++ index.js
@@ -11,3 +11,79 @@ console.log(lastName);
 interestRate = 32;
 
 console.log(interestRate);
+
+let grade = 'A';
+let number =32;
+let isName = false;
+
+let adddress = undefined;
+let selectedColor = null;
+
+
+let perosn = {nname:'sandun',aage:43};
+
+//dot notation
+perosn.aage = 22;
+perosn ['nname'] = 'nimal';
+
+console.log(perosn.nname);
+
+
+//arrays
+
+let games = ['cricket','football','bascketball','vollyball'];
+
+games[4] = 32;
+games[2] = 'carom';
+console.log(games[2]);
+console.log(games.length);
+
+//function in js
+
+function greet(namE,lastname){
+    console.log('Hello '+' '+namE +' '+ lastname);
+}
+
+greet('purna','lakshitha');
+greet('anura','nishantha');
+
+//calculation values function 
+function squre(num){
+    return num*num;
+}
+
+let a = squre(7);
+console.log(a);
+
+//another method
+console.log(squre(3));
+
+console.log('sa');
+
+//arithmetic operators
+let x = 10;
+let y = 3;
+
+// console.log(x+y);
+// console.log(x-y);
+// console.log(x*y);
+// console.log(x/y);
+// console.log(x%y);
+
+
+
+// x = x + 5;  
+
+// x += 5;
+
+
+//increment
+console.log(x++); //10//passta ghuwoth baba hamben na
+console.log(x);//11
+
+x = 10;
+
+//decrement
+console.log(--x);//9
+console.log(x)//9
+
---
+++ index2.js
@@ -0,0 +1,29 @@
+
+//====================assignment operators
+
+
+console.log('meka aluth eka');
+
+// x = x+5;
+// x += 5;
+
+// x = x-4;
+// x -=4;
+
+let y = 10;
+
+//relational
+console.log(y>0);
+console.log(y<=10);
+console.log(y>0);
+console.log(y>0);
+console.log(y>0);
+
+//Equality
+console.log(y===10);
+console.log(y!==10);
+
+
+
+ 
+
