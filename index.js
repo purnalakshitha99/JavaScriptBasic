f3a718dc6b59db8940a08b3ae1694f860737eaf3
Purna Lakshitha <99383161+purnalakshitha99@users.noreply.github.com>
2022-12-12 15:31:29 +0530

    variable

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello sworld');
+let firstName = 'purna';
+console.log(firstName);
+
+let lastName = 'lakshitha', middleName = 'ekanayaka';
+
+console.log(middleName);
+console.log(lastName);
+
+ interestRate = 0.3;
+
+interestRate = 32;
+
+console.log(interestRate);
